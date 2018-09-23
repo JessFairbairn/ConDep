9621f3379a967ce9af3e514a8c6e5dee0383a637
Jessica Fairbairn <jess.fairbairn@postgrad.manchester.ac.uk>
2018-09-23 21:19:01 +0100

    Got prolog setup working

---
+++ prolog/emit_test.pl
@@ -0,0 +1,11 @@
+:- include(general).
+% :- discontiguous(subject).
+% :- discontiguous(object).
+
+expelEvent(myEvent).
+
+subject(myEvent, blackHole).
+object(myEvent, barbara).
+
+% isTime(timeAfterEvent).
+justAfter(timeAfterEvent, myEvent).
--- prolog/general.pl
+++ prolog/general.pl
@@ -1,6 +1,36 @@
-% isPartOf(_Part,_Thing).
+% Event defintions
+% event(_).
+injestEvent(_).
+event(B) :- expelEvent(B).
+event(B) :- injestEvent(B).
+
+
+isTime(_).
+% justAfter(_time,_event).
+
+% subject(_Event, _subject).
+% object(_event,_subject).
+
+% Physical definitions
+% inside(_food,_eater,_time).
+inside(Food,Eater,T) :-
+    isTime(T),
+    injestEvent(InjEvent),
+    justAfter(T,InjEvent),
+    subject(InjEvent, Eater),
+    object(InjEvent, Food).
+
+
+% outside(_innerThing, _container, _time).
+assert(not(
+    inside(A,B,T),
+    outside(A,B,T)
+)).
+
+% partOf(_Part,_Thing).
 % ptrans(_).
 
-move(Thing, Part) :- isPartOf(Part,Thing), ptrans(Part).
+% move(Thing, Part) :- isPartOf(Part,Thing), ptransEvent(Part).
 
-collapse(Thing, Radius) :- .
+
+
